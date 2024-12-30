c0bdf3cbfb325c9f52a7797f5021530b07086627
rainkrisn <aathrey.krishnan@gmail.com>
2024-12-30 07:13:24 -0800

    base of the program

--- actual-code.py
+++ actual-code.py
@@ -1,1 +1,24 @@
-#hi
+from tkinter import *
+from tkinter import ttk
+import tkinter.messagebox as messagebox
+
+root = Tk()
+root.geometry("1500x700")
+root.title("BanranApp")
+
+root.overrideredirect(True)
+
+#root.attributes('-alpha', 0.99)
+
+#root.wm_attributes('-transparentcolor', 'white')
+
+
+main_frame = Frame(root, bg='#f0f0f0', highlightthickness=2)
+main_frame.pack(fill=BOTH, expand=True, padx=10, pady=10)
+
+close_button = Button(main_frame, text='X', command=root.destroy, 
+                     bg='#f0f0f0', bd=0, padx=10)
+close_button.pack(anchor='ne')
+
+root.mainloop()
+
