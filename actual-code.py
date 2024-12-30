33b03b11f5fbc579bdb55808bcc7bbb3eab354cf
rainkrisn <aathrey.krishnan@gmail.com>
2024-12-30 07:31:30 -0800

    made it more aesthetic

--- actual-code.py
+++ actual-code.py
@@ -1,6 +1,4 @@
 from tkinter import *
-from tkinter import ttk
-import tkinter.messagebox as messagebox
 
 root = Tk()
 root.geometry("1500x700")
@@ -17,8 +15,13 @@ main_frame = Frame(root, bg='#f0f0f0', highlightthickness=2)
 main_frame.pack(fill=BOTH, expand=True, padx=10, pady=10)
 
 close_button = Button(main_frame, text='X', command=root.destroy, 
-                     bg='#f0f0f0', bd=0, padx=10)
+                     bg='#f0f0f0', bd=0, padx=10,
+                     font=('Arial', 14),
+                     width=3, height=1)
 close_button.pack(anchor='ne')
+
+welcome = Label(root, text="Rainy", font=('Source Code Pro',20,'italic'))
+welcome.place(x=10, y=10)
 
 root.mainloop()
 
